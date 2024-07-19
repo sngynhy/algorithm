a98293e3bd2b7d52ccb35196271106905942be8c
sngynhy <sngynhy@gmail.com>
2024-07-19 15:30:33 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.32 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,9 +1,10 @@
 function solution(arr1, arr2) {
-    let answer = Array.from({length: arr1.length}, () => new Array(1))
-    for (let i=0; i<arr1.length; i++) {
-        for (let j=0; j<arr1[i].length; j++) {
-            answer[i][j] = arr1[i][j] + arr2[i][j]
-        }
-    }
-    return answer;
+    // let answer = Array.from({length: arr1.length}, () => new Array(1))
+    // for (let i=0; i<arr1.length; i++) {
+    //     for (let j=0; j<arr1[i].length; j++) {
+    //         answer[i][j] = arr1[i][j] + arr2[i][j]
+    //     }
+    // }
+    // return answer;
+    return arr1.map((el, i) => el.map((val, j) => val + arr2[i][j]))
 }
