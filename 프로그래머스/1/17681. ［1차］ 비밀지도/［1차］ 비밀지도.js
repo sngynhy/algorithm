509470d9f887f5736be640235f8885e8279a1bf0
sngynhy <sngynhy@gmail.com>
2024-07-22 17:40:19 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,14 +1,18 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    let decimal1 = [], decimal2 = []
-    for (let i=0; i<arr1.length; i++) {
-        const dec1 = arr1[i].toString(2).padStart(n, '0')
-        const dec2 = arr2[i].toString(2).padStart(n, '0')
-        let str = ''
-        for (let j=0; j<dec1.length; j++) {
-            str += !Number(dec1[j]) && !Number(dec2[j]) ? ' ' : '#'
-        }
-        answer.push(str)
-    }
-    return answer;
+    // var answer = [];
+    // let decimal1 = [], decimal2 = []
+    // for (let i=0; i<arr1.length; i++) {
+    //     const dec1 = arr1[i].toString(2).padStart(n, '0')
+    //     const dec2 = arr2[i].toString(2).padStart(n, '0')
+    //     let str = ''
+    //     for (let j=0; j<dec1.length; j++) {
+    //         str += !Number(dec1[j]) && !Number(dec2[j]) ? ' ' : '#'
+    //     }
+    //     answer.push(str)
+    // }
+    // return answer;
+    
+    return arr1.map((el, i) => {
+        return (el|arr2[i]).toString(2).padStart(n, '0').replace(/0/g, ' ').replace(/1/g, '#')
+    })
 }
