77ead759d7337189a32bf867402494858c5892f8
sngynhy <sngynhy@gmail.com>
2024-07-22 12:45:17 +0900

    [level 1] Title: K번째수, Time: 2.84 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,7 @@
 function solution(array, commands) {
-    var answer = [];
-    commands.map((el, index) => {
+    return commands.map((el, index) => {
         let [i, j, k] = el
-        answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1])
-    })
-    return answer;
+        console.log(i,j,k)
+        return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
+    });
 }
