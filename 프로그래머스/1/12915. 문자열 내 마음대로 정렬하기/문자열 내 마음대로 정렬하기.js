67c78c465bb95a0f4b51f52ab384997d2094f9ad
sngynhy <sngynhy@gmail.com>
2024-07-23 14:40:06 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 47.72 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -2,4 +2,6 @@ function solution(strings, n) {
     return strings.sort((a, b) => {
         return a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n])
     })
+    // 'a'.localeCompare('b')
+    // 사전 정렬 기준 a가 b보다 빠르면 -1, 느리면 1, 같으면 0 반환
 }
