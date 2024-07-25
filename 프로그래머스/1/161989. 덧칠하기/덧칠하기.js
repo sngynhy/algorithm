6c91e8ce65f8e03ca28f799a929b12a16f1bce12
sngynhy <sngynhy@gmail.com>
2024-07-25 14:36:12 +0900

    [level 1] Title: 덧칠하기, Time: 5.23 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,13 +1,20 @@
 function solution(n, m, section) {
     var answer = 0;
-    let max = Math.max(...section), min = Math.min(...section)
-    while (min <= max) {
-        if (!section.includes(min)) {
-            min++
-            continue
+    // let max = Math.max(...section), min = Math.min(...section)
+    // while (min <= max) {
+    //     if (!section.includes(min)) {
+    //         min++
+    //         continue
+    //     }
+    //     answer++
+    //     min += m
+    // }
+    var painted = 0
+    for(var s of section) {
+        if(painted < s) {
+            answer++
+            painted = s + m - 1
         }
-        answer++
-        min += m
     }
     return answer;
 }
