7c7f3098692f3f530c2eabdebbcd5c0b473f71f9
sngynhy <sngynhy@gmail.com>
2024-07-29 14:17:46 +0900

    [level 1] Title: 실패율, Time: 1645.54 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,10 +1,9 @@
 function solution(N, stages) {
     var answer = [];
-    let len = stages.length
     let cnt = 0
     for (let i=1; i<=N; i++) {
         let res = stages.filter(el => el === i).length
-        answer.push({key: i, value: res / (len - cnt)})
+        answer.push({key: i, value: res / (stages.length - cnt)})
         cnt += res
     }
     return answer.sort((a, b) => b.value - a.value).map(el => el.key);
