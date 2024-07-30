af1710d1491cf063027edba442ba6134abec808a
sngynhy <sngynhy@gmail.com>
2024-07-30 15:57:32 +0900

    Update 완주하지 못한 선수.js

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,12 @@
 function solution(participant, completion) {
     let map = new Map()
-    for (const p of completion) {
+    for (const p of participant) {
         map.set(p, map.get(p) ? map.get(p) + 1 : 1)
     }
-    for (const p of participant) {
-        if (!map.has(p) || map.get(p) < 1) return p
+    for (const p of completion) {
         map.set(p, map.get(p) - 1)
+    }
+    for (const [key, val] of map) {
+        if (val > 0) return key
     }
 }
