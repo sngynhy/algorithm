e95d79d983b2f54c415ce34e8e38cdf8bc396bec
sngynhy <sngynhy@gmail.com>
2024-08-08 15:50:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 16.60 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 1.40 ms
+메모리: 36.7 MB, 시간: 16.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 30일 17:06:54
+2024년 08월 08일 15:50:23
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,14 +1,18 @@
 function solution(keymap, targets) {
-    var answer = [];
+    var answer = []
     let map = new Map()
-    keymap.forEach(str => {
-        str.split('').forEach((c, j) => {
-            let index = map.get(c) 
-            !index ? map.set(c, j+1) : index > j+1 ? map.set(c, j+1) : null
+    keymap.forEach(el =>{
+        el.split('').forEach((c, i) => {
+            if (!map.has(c) || map.has(c) && map.get(c) > i + 1) map.set(c, i + 1)
         })
     })
-    targets.forEach(target => {
-        answer.push(target.split('').reduce((acc, curr) => acc += map.get(curr), 0) || -1)
+    targets.forEach(el => {
+        let flag = false
+        let res = el.split('').reduce((acc, curr) => {
+            map.has(curr) ? acc += map.get(curr) : flag = true
+            return flag ? -1 : acc
+        }, 0)
+        answer.push(res)
     })
-    return answer;
+    return answer
 }
