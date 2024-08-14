3add993111627e9b31cbd4c8360681658bff4cc9
sngynhy <sngynhy@gmail.com>
2024-08-14 20:16:33 +0900

    [level 1] Title: 신고 결과 받기, Time: 352.48 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,14 @@
+function solution(id_list, report, k) {
+    var answer = Array(id_list.length).fill(0)
+    report = [...new Set(report)]
+    let res = {}
+    report.map(el => {
+        const arr = el.split(' ')
+        res[arr[1]] ? res[arr[1]].push(arr[0]) : res[arr[1]] = [arr[0]]
+    })
+    let stopMember = Object.keys(res).filter(el => res[el].length >= k)
+    for (const member of stopMember) {
+        res[member].forEach(el => answer[id_list.indexOf(el)] += 1)
+    }
+    return answer;
+}
