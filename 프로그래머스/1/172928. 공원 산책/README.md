6e0a8ad8fc8eae3d68e27296e99b21b787b7a781
sngynhy <sngynhy@gmail.com>
2024-08-16 17:45:23 +0900

    [level 1] Title: 공원 산책, Time: 0.69 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928#qna) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.55 ms
+메모리: 33.7 MB, 시간: 0.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 17:43:16
+2024년 08월 16일 17:45:20
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,45 +1,23 @@
 function solution(park, routes) {
-    let start = [] // 시작 위치
-    for (let i=0; i<park.length; i++) {
-        let index = park[i].indexOf('S')
-        if (index > -1) {
-            start = [i, index]
-            break
+        const dirs = { E: [0, 1], W: [0, -1], S: [1, 0], N: [-1, 0] };
+        let [x, y] = [0, 0];
+        for (let i = 0; i < park.length; i++) {
+          if (park[i].includes('S')) {
+            [x, y] = [i, park[i].indexOf('S')];
+            break;
+          }
         }
-    }
-    
-    let prev = [...start], curr = [] // 이전 위치, 현재 위치
-    routes.forEach(el => {
-        let dir = el.split(' ')[0], dis = Number(el.split(' ')[1]) // 방향, 거리
-        let flag = true
-        for (let j=1; j<=dis; j++) {
-            curr = move(prev, dir)
-            let res = park[curr[0]] ? park[curr[0]][curr[1]] : undefined
-            if (res === 'X' || res === undefined) {
-                flag = false
-                break
-            }
-            prev = [...curr]
-        }
-        flag ? start = curr : prev = [...start]
-    })
-    return start;
-}
 
-function move (curr, dir) { // 현재위치, 방향
-    switch (dir) {
-        case 'N':
-            curr[0] -= 1
-            break
-        case 'S':
-            curr[0] += 1
-            break
-        case 'W':
-            curr[1] -= 1
-            break
-        case 'E':
-            curr[1] += 1
-            break
-    }
-    return curr
-}
+        routes.forEach((route) => {
+          const [r, n] = route.split(' ');
+          let [nx, ny] = [x, y];
+          let cnt = 0;
+          while (cnt < n) {
+            [nx, ny] = [nx + dirs[r][0], ny + dirs[r][1]];
+            if (!park[nx] || !park[nx][ny] || park[nx][ny] === 'X') break;
+            cnt++;
+          }
+          if (cnt == n) [x, y] = [nx, ny];
+        });
+        return [x, y];
+      }
