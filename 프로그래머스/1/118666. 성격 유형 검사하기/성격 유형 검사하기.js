e17233b94d1c19552acad5b307d95509026463e3
sngynhy <sngynhy@gmail.com>
2024-08-18 12:57:55 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,30 @@
+function solution(survey, choices) {
+    var answer = '';
+    // 성격 유형
+    let type = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']]
+    // 성격 유형 점수
+    let score = [3, 2, 1, 0, 1, 2, 3]
+    let map = new Map()
+    survey.forEach((el, i) => {
+        let temp = 0
+        switch (choices[i]) {
+            case 1:
+            case 2:
+            case 3:
+                temp = map.get(el[0]) || 0
+                map.set(el[0], temp + score[choices[i]-1])
+                break
+            case 5:
+            case 6:
+            case 7:
+                temp = map.get(el[1]) || 0
+                map.set(el[1], temp + score[choices[i]-1])
+                break
+        }
+    })
+    type.forEach(el => {
+        let score1 = map.get(el[0]) || 0, score2 = map.get(el[1]) || 0
+        answer += score1 >= score2 ? el[0] : el[1]
+    })
+    return answer;
+}
