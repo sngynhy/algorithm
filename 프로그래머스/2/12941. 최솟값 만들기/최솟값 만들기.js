955dfeb093ccb20a942f15b57d929b47696e3fd2
sngynhy <sngynhy@gmail.com>
2024-08-22 20:34:57 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.70 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,5 @@
 function solution(A, B){
-    A = A.sort((a, b) => a - b)
-    B = B.sort((a, b) => b - a)
-    return A.reduce((acc, curr, i) => {
-        return acc += curr * B[i]
-    }, 0);
+    A.sort((a, b) => a - b)
+    B.sort((a, b) => b - a)
+    return A.reduce((acc, curr, i) => acc += curr * B[i], 0);
 }
