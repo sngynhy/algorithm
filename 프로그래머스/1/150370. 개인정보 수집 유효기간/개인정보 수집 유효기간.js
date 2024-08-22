58082f213396e6e25e0f7327061e447695fa72ed
sngynhy <sngynhy@gmail.com>
2024-08-22 14:43:17 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.90 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,48 +1,21 @@
-function solution(today, terms, privacies) {
-    // #1
-    // 1 > 2021.05.02 A => 2021년 11월 1일까지 => 파기
-    // 2 > 2021.07.01 B => 2022년 6월 28일까지
-    // 3 > 2022.02.19 C => 2022년 5월 18일까지 => 파기
-    // 4 > 2022.02.20 C => 2022년 5월 19일까지
-    // #2
-    // 1 > 2019.01.01 D => 2019년 5월 28일까지 보관 => 파기
-    // 2 > 2019.11.15 Z => 2020년 2월 14일까지 보관
-    // 3 > 2019.08.02 D => 2020년 1월 1일까지 보관
-    // 4 > 2019.07.01 D => 2019년 11월 28일까지 보관 => 파기
-    // 5 > 2018.12.28 Z => 2019년 3월 27일까지 보관 => 파기
-    
+function solution(today, terms, privacies) {  
     var answer = [];
     let period = new Map()
     terms.forEach(el => period.set(el.split(' ')[0], Number(el.split(' ')[1])))
-    privacies.forEach((el, i) => {
-        let type = el.split(' ')[1], tempDate = getDate(el.split(' ')[0])
-        
-        tempDate.date -= 1
-        if (tempDate.date === 0) {
-            tempDate.date = 28
-            tempDate.month -= 1
-        }
+     privacies.forEach((el, i) => {
+        let type = el.split(' ')[1], [year, month, date] = el.split(' ')[0].split('.').map(Number)
         
         let months = period.get(type)
-        if (months > 12) tempDate.year += Math.trunc(months / 12), tempDate.month += months % 12
-        else tempDate.month += months
+        if (months > 12) year += Math.trunc(months / 12), month += months % 12
+        else month += months
         
-        if (tempDate.month > 12) {
-            tempDate.year += 1
-            tempDate.month -= 12
+        if (month > 12) {
+            year += 1
+            month -= 12
         }
         
-        if (new Date(today) > new Date(tempDate.year + '.' + tempDate.month + '.' + tempDate.date)) {
-            answer.push(i+1)
-        }
+        let expiresDate = [year, month, date].join('.')
+        if (new Date(today) >= new Date(expiresDate)) answer.push(i+1)
     })
     return answer;
-}
-
-function getDate (date) {
-    return {
-        year: new Date(date).getYear() + 1900,
-        month: new Date(date).getMonth() + 1,
-        date: new Date(date).getDate()
-    }
 }
