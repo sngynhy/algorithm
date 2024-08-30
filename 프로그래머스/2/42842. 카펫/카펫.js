ab998685ea973ea4db9edb04048f0dbd963a804d
sngynhy <sngynhy@gmail.com>
2024-08-30 14:33:57 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,21 +1,13 @@
 function solution(brown, yellow) {
-    let answer = [];
-    // w >= h
-    // w * h = yellow => w, h는 yellow의 약수
-    // w + h <= (brown - 4) / 2
-    let w = [], h = []
-    for (let i=1; i<=Math.sqrt(yellow); i++) {
-        if (yellow % i === 0) {
-            h.push(i)
-            w.push(yellow / i)
-        }
-    }
-    
-    for (let i=0; i<w.length; i++) {
-        if (w[i]+h[i] === (brown-4)/2) {
-            answer.push(w[i]+2)
-            answer.push(h[i]+2)
-        }
-    }
-    return answer;
+    // 1) S => w * h = brown + yellow (w >= h)
+    // 2) brown = 2w + 2(h - 2) => h = (brown - 2w + 4) / 2
+    // 2번식을 1번식의 h에 대입하여 S를 w에 대한 식으로 나타내면
+    // w((brown - 2w + 4) / 2) = brown + yellow
+    // 위의 식을 w에 대한 이차 방정식으로 표현하면
+    // w^2 - (2 + 0.5 * brown)w + (brown + yellow) = 0
+    // 근의 공식을 이용하여 w값 구하기
+    let a = 1, b = (2 + 0.5 * brown), c = brown + yellow
+    let w1 = (b + Math.sqrt(Math.pow(b, 2) - (4*a*c))) / 2
+    let w2 = (b - Math.sqrt(Math.pow(b, 2) - (4*a*c))) / 2
+    return [w1, w2];
 }
