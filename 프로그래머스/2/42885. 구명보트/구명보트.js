4eea60ff4a8a1bb8f2b765607a148c628d922c64
sngynhy <sngynhy@gmail.com>
2024-09-02 12:39:56 +0900

    [level 2] Title: 구명보트, Time: 34.60 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,15 +1,20 @@
 function solution(people, limit) {
-    var answer = 0;
     people.sort((a, b) => a - b)
-    let min = 0, max = people.length - 1
-    while (min <= max) {
-        if (people[min] + people[max] <= limit) {
-            min++
-            max--
-        } else {
-            max--
-        }
-        answer++
+    for (var min=0, max=people.length - 1; min<max; max--) {
+        if (people[min] + people[max] <= limit) min++
     }
-    return answer;
+    return people.length - min;
+    
+    // var answer = 0;
+    // let min = 0, max = people.length - 1
+    // while (min <= max) {
+    //     if (people[min] + people[max] <= limit) {
+    //         min++
+    //         max--
+    //     } else {
+    //         max--
+    //     }
+    //     answer++
+    // }
+    // return answer;
 }
