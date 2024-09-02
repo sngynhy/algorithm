69ce611abfc22e0dc86b7b55f13f212e405ce2f3
sngynhy <sngynhy@gmail.com>
2024-09-02 12:42:35 +0900

    [level 2] Title: 구명보트, Time: 27.57 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,5 +1,6 @@
 function solution(people, limit) {
     people.sort((a, b) => a - b)
+    // 보트엔 2명만 탈 수 있음 => 가장 가벼운 사람과 가장 무거운 사람
     for (var min=0, max=people.length - 1; min<max; max--) {
         if (people[min] + people[max] <= limit) min++
     }
