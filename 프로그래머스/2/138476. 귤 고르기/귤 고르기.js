10f93d002d7ba996f705fac7d1e0259fd561e026
sngynhy <sngynhy@gmail.com>
2024-11-11 21:48:07 +0900

    [level 2] Title: 귤 고르기, Time: 115.99 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,11 +1,9 @@
 function solution(k, tangerine) {
     var answer = 0;
-    // const obj = tangerine.reduce((acc, curr) => {
-    //     acc[curr] = (acc[curr] || 0) + 1
-    //     return acc
-    // }, {})
-    const obj = {} 
-    tangerine.forEach(el => obj[el] = (obj[el] || 0) + 1)
+    const obj = tangerine.reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1
+        return acc
+    }, {})
     const arr = Object.values(obj).sort((a, b) => b - a)
     for (const cnt of arr) {
         answer++
