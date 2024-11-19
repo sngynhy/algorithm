99cd047a393d8f8d302cfb8156cb6a622b043b71
sngynhy <sngynhy@gmail.com>
2024-11-19 11:25:14 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 8622.04 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,13 @@
+function solution(elements) {
+    let set = new Set()
+    for (let i=0, n=1; i<elements.length; i++, n++) {
+        for (let j=0; j<elements.length; j++) {
+            let sum = [...elements, ...elements].slice(j, j+n).reduce((acc, curr) => {
+                return acc + curr
+            }, 0)
+            if (!set.has(sum)) set.add(sum)
+            if (i === elements.length-1) break
+        }
+    }
+    return set.size;
+}
