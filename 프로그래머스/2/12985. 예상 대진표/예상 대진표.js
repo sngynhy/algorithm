836768037ffc1937e4530aaa5e14984766ccadf7
sngynhy <sngynhy@gmail.com>
2024-11-19 22:20:54 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,10 +1,15 @@
 function solution(n,a,b) {
     let answer = 0
-    for (let i=0; i<Math.log2(n); i++) {
-        if (a === b) break
-        a = Math.ceil(a/2)
-        b = Math.ceil(b/2)
-        answer++
+    // for (let i=0; i<Math.log2(n); i++) {
+    //     if (a === b) break
+    //     a = Math.ceil(a/2)
+    //     b = Math.ceil(b/2)
+    //     answer++
+    // }
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        answer++;
     }
     return answer
 }
